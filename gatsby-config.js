9fefbf56db43bb15d8f5ba2d8f376abef8fd27db
Jarod Peachey <jwpeachey107@aol.com>
2020-04-07 13:29:09 -0400

    Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const theme = require('./content/settings/theme.json');
 const site = require('./content/settings/site.json');
 
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
+  path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
@@ -16,47 +16,58 @@ module.exports = {
       resolve: 'gatsby-plugin-tinacms',
       options: {
         sidebar: {
-          hidden: typeof window !== 'undefined' ? window.location.pathname === '/tests' : false,
+          hidden:
+            typeof window !== 'undefined'
+              ? window.location.pathname === '/tests'
+              : false,
           position: 'displace',
           theme: {
             color: {
               primary: {
                 light: theme.color.primary,
                 medium: theme.color.primary,
-                dark: theme.color.primary
-              }
-            }
-          }
+                dark: theme.color.primary,
+              },
+            },
+          },
         },
-        plugins: ['new-gatsby-tinacms-git', 'gatsby-tinacms-remark']
-      }
+        plugins: [
+          {
+            resolve: 'gatsby-tinacms-git',
+            options: {
+              sshKey: process.env.SSH_KEY,
+            },
+          },
+          'gatsby-tinacms-remark',
+        ],
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/images`,
-        name: 'images'
-      }
+        name: 'images',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'content',
-        path: `${__dirname}/content`
-      }
+        path: `${__dirname}/content`,
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'settings',
-        path: `${__dirname}/content/settings`
-      }
+        path: `${__dirname}/content/settings`,
+      },
     },
     {
       resolve: `gatsby-plugin-layout`,
       options: {
-        component: require.resolve(`./src/components/siteLayout.js`)
-      }
+        component: require.resolve(`./src/components/siteLayout.js`),
+      },
     },
     'gatsby-plugin-react-helmet',
     {
@@ -68,8 +79,8 @@ module.exports = {
         background_color: theme.color.primary,
         theme_color: theme.color.primary,
         display: 'minimal-ui',
-        icon: 'content/images/icon.png'
-      }
+        icon: 'content/images/icon.png',
+      },
     },
     'gatsby-plugin-offline',
     {
@@ -79,21 +90,21 @@ module.exports = {
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
-              name: 'uploads'
-            }
+              name: 'uploads',
+            },
           },
           {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 880,
-              withWebp: true
-            }
+              withWebp: true,
+            },
           },
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: 'static'
-            }
+              destinationDir: 'static',
+            },
           },
           {
             resolve: 'gatsby-remark-prismjs',
@@ -106,20 +117,20 @@ module.exports = {
               prompt: {
                 user: 'root',
                 host: 'localhost',
-                global: false
-              }
-            }
-          }
-        ]
-      }
+                global: false,
+              },
+            },
+          },
+        ],
+      },
     },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['Lato:400,700']
-        }
-      }
+          families: ['Lato:400,700'],
+        },
+      },
     },
     {
       resolve: 'gatsby-plugin-material-ui',
@@ -129,19 +140,19 @@ module.exports = {
         },
       },
     },
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
   ],
   // for avoiding CORS while developing Netlify Functions locally
   // read more: https://www.gatsbyjs.org/docs/api-proxy/#advanced-proxying
-  developMiddleware: app => {
+  developMiddleware: (app) => {
     app.use(
       '/.netlify/functions/',
       proxy({
         target: 'http://localhost:9000',
         pathRewrite: {
-          '/.netlify/functions/': ''
-        }
+          '/.netlify/functions/': '',
+        },
       })
     );
-  }
+  },
 };
