b08a1125584cbda73707b10daea137e8c8384188
Jarod Peachey <jwpeachey107@aol.com>
2020-04-11 12:45:14 -0400

    Updates

--- src/templates/post.js
+++ src/templates/post.js
@@ -735,20 +735,5 @@ export const postQuery = graphql`
       name
       email
     }
-    allNetlifySubmissions(filter: { data: { path: { eq: $path } } }) {
-      edges {
-        node {
-          number
-          data {
-            comment
-            email
-            name
-            path
-            parentCommentNumber
-          }
-          created_at(formatString: "M/D/YYYY")
-        }
-      }
-    }
   }
 `;
